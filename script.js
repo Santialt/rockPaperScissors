db21b41ae24427f2f04b4f4a6f128312d87818ea
santiago <altamirano.santi18@gmail.com>
2021-05-27 18:47:16 -0300

    new version

--- script.js
+++ script.js
@@ -9,57 +9,57 @@ function playRound(){
     let computerSelection = computerPlay()
     
     if (playerSelection === 'rock' && computerSelection === 'rock'){
-       console.log('player selection: ' + playerSelection);
-       console.log('computer selection: ' + computerSelection);
-        console.log('draw');
-        console.log('player score: ' + playerScore + ' computer score: ' + computerScore);
+       alert('player selection: ' + playerSelection);
+       alert('computer selection: ' + computerSelection);
+        alert('draw');
+        alert('player score: ' + playerScore + ' computer score: ' + computerScore);
     }else if (playerSelection === 'rock' && computerSelection === 'paper'){
-        console.log('player selection: ' + playerSelection);
-        console.log('computer selection: ' + computerSelection);
-        console.log('computer win');
+        alert('player selection: ' + playerSelection);
+        alert('computer selection: ' + computerSelection);
+        alert('computer win');
         computerScore++;
-        console.log('player score: ' + playerScore + ' computer score: ' + computerScore);
+        alert('player score: ' + playerScore + ' computer score: ' + computerScore);
     }else if (playerSelection === 'rock' && computerSelection === 'scissors'){
-       console.log('player selection: ' + playerSelection);
-       console.log('computer selection: ' + computerSelection);
-        console.log('player win');
+       alert('player selection: ' + playerSelection);
+       alert('computer selection: ' + computerSelection);
+        alert('player win');
         playerScore++;
-        console.log('player score: ' + playerScore + ' computer score: ' + computerScore);
+        alert('player score: ' + playerScore + ' computer score: ' + computerScore);
     }else if (playerSelection === 'paper' && computerSelection === 'paper'){
-       console.log('player selection: ' + playerSelection);
-       console.log('computer selection: ' + computerSelection);
-        console.log('draw');
-        console.log('player score: ' + playerScore + ' computer score: ' + computerScore);
+       alert('player selection: ' + playerSelection);
+       alert('computer selection: ' + computerSelection);
+        alert('draw');
+        alert('player score: ' + playerScore + ' computer score: ' + computerScore);
     }else if (playerSelection === 'paper' && computerSelection === 'scissors'){
-        console.log('player selection: ' + playerSelection);
-        console.log('computer selection: ' + computerSelection);
-        console.log('computer win');
+        alert('player selection: ' + playerSelection);
+        alert('computer selection: ' + computerSelection);
+        alert('computer win');
         computerScore++;
-        console.log('player score: ' + playerScore + ' computer score: ' + computerScore);
+        alert('player score: ' + playerScore + ' computer score: ' + computerScore);
     }else if (playerSelection === 'scissors' && computerSelection === 'scissors'){
-       console.log('player selection: ' + playerSelection);
-       console.log('computer selection: ' + computerSelection);
-        console.log('draw');
-        console.log('player score: ' + playerScore + ' computer score: ' + computerScore);
+       alert('player selection: ' + playerSelection);
+       alert('computer selection: ' + computerSelection);
+        alert('draw');
+        alert('player score: ' + playerScore + ' computer score: ' + computerScore);
     } else if (playerSelection === 'paper' && computerSelection === 'rock'){
-        console.log('player selection: ' + playerSelection);
-        console.log('computer selection: ' + computerSelection);
-         console.log('player win');
+        alert('player selection: ' + playerSelection);
+        alert('computer selection: ' + computerSelection);
+         alert('player win');
          playerScore++;
-         console.log('player score: ' + playerScore + ' computer score: ' + computerScore);
+         alert('player score: ' + playerScore + ' computer score: ' + computerScore);
     }else if (playerSelection === 'scissors' && computerSelection === 'rock'){
-        console.log('player selection: ' + playerSelection);
-        console.log('computer selection: ' + computerSelection);
-         console.log('computer win');
+        alert('player selection: ' + playerSelection);
+        alert('computer selection: ' + computerSelection);
+         alert('computer win');
          computerScore++;
-         console.log('player score: ' + playerScore + ' computer score: ' + computerScore);
+         alert('player score: ' + playerScore + ' computer score: ' + computerScore);
     }
     else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-        console.log('player selection: ' + playerSelection);
-        console.log('computer selection: ' + computerSelection);
-         console.log('player win');
+        alert('player selection: ' + playerSelection);
+        alert('computer selection: ' + computerSelection);
+         alert('player win');
          playerScore++;
-         console.log('player score: ' + playerScore + ' computer score: ' + computerScore);
+         alert('player score: ' + playerScore + ' computer score: ' + computerScore);
     }
 }
 
